27d69d4763384539dbc3ddae62f4c9947e6ff221
kissartisan <kissartisan@pm.me>
2023-06-02 17:44:52 +0800

    Ep. 3 > Trending Articles with Sorted Sets

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
 
@@ -25,4 +26,25 @@ Route::get('/videos/{id}/download', function ($id) {
     Redis::incr("videos.{$id}.downloads");
 
     return back();
+});
+
+Route::get('/articles/trending', function (Article $article) {
+    $trending = Redis::zrevrange('trending_articles', 0, 2);
+
+    $trending = Article::hydrate(
+        array_map('json_decode', $trending)
+    );
+
+    return $trending;
+});
+
+Route::get('/articles/{article}', function (Article $article) {
+    Redis::zincrby('trending_articles', 1, $article);
+
+    // Remove the items that are not in the top 3
+    // This code is the clean up Redis value if they got bloated.
+    // This can be run on a CRON JOB to eventually remove items from Redis
+    // Redis::zremrangebyrank('trending_articles', 0, -4);
+
+    return $article;
 });
